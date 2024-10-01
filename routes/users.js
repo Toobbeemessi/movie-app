b9f5db12e734f126b2735846b6c1366a1171c58b
Toobbeemessi <160336976+Toobbeemessi@users.noreply.github.com>
2024-10-01 23:00:17 +0100

    Add files via upload

---
+++ routes/users.js
@@ -0,0 +1,29 @@
+const { User, validate } = require("../models/user");
+const express = require("express");
+const router = express.Router();
+const _ = require("lodash");
+const bcrypt = require("bcrypt");
+const config = require("config");
+const jwt = require("jsonwebtoken");
+
+router.post("/", async (req, res) => {
+  const { error } = validate(req.body);
+
+  if (error) return res.status(400).send(error.details[0].message);
+  let user = await User.findOne({ email: req.body.email });
+  if (user) return res.status(400).send("User already registered.");
+
+  user = new User(_.pick(req.body, ["name", "email", "password"]));
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(user.password, salt);
+
+  const token = jwt.sign(
+    { _id: user._id, name: user.name },
+    config.get("jwtPrivateKey")
+  );
+
+  await user.save();
+  res.header('x-auth-token', token).send(_.pick(user, ["_id", "name", "email"]));
+});
+
+module.exports = router;
